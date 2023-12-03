793fb53561b535457e70f7016c5cde5859007548
ian <ikusner13@gmail.com>
2023-12-03 09:35:11 -0500

    small optimize

--- day3/p1.py
+++ day3/p1.py
@@ -2,26 +2,13 @@ D = open(0).read()
 
 S = ["*", '+', '$', "#", "/", "@", "%", '=', "&", "-"]
 
-def check_adjacent(start, end, line, lines, index):
-  # check next to
-  if (start > 0 and line[start] in S) or (end < len(line) - 1 and line[end] in S):
-    return True
-
-  # check above and below
-  for i in range(start, end + 1):
-    above = index > 0 and lines[index -1][i] in S
-    below = index < len(line) - 1 and lines[index + 1][i] in S
-    if above or below:
-      return True
-
-
-
 def part1():
   lines = D.splitlines()
 
   part_numbers = []
 
   for index, line in enumerate(lines):
+    line_end = len(line)-1
     number_start = -1
     number_end = -1
     for line_index, char in enumerate(line):
@@ -34,30 +21,27 @@ def part1():
       if number_start == -1 and number_end == -1:
         continue
 
-      if char == "." or line_index == len(line) - 1 or char in S:
+      if char == "." or line_index == line_end or char in S:
         full_number = int(line[number_start:number_end+1])
 
         check_start = max(number_start - 1, 0)
-        check_end = min(number_end + 1, len(line) - 1)
+        check_end = min(number_end + 1, line_end)
         
         is_adj = False
 
         if (check_start > 0 and line[check_start] in S):
           is_adj = True
-        elif(check_end < len(line) - 1 and line[check_end] in S):
+        elif(check_end < line_end and line[check_end] in S):
           is_adj = True
 
         # check above and below
         for i in range(check_start, check_end + 1):
           above = index > 0 and lines[index -1][i] in S
-          below = index < len(line) - 1 and lines[index + 1][i] in S
-          if above:
+          below = index < line_end and lines[index + 1][i] in S
+          if above or below:
             is_adj = True
             break
-          if below:
-            is_adj = True
-            break
-
+          
         if is_adj:
           part_numbers.append(full_number)
 
--- day3/p2.py
+++ day3/p2.py
@@ -10,6 +10,7 @@ def part2():
   part_numbers_dict = defaultdict(list)
 
   for index, line in enumerate(lines):
+    line_end = len(line)-1
     number_start = -1
     number_end = -1
     for line_index, char in enumerate(line):
@@ -22,11 +23,11 @@ def part2():
       if number_start == -1 and number_end == -1:
         continue
 
-      if char == "." or line_index == len(line) - 1 or char in S:
+      if char == "." or line_index == line_end or char in S:
         full_number = int(line[number_start:number_end+1])
 
         check_start = max(number_start - 1, 0)
-        check_end = min(number_end + 1, len(line) - 1)
+        check_end = min(number_end + 1, line_end)
         
         is_adj = False
         adj_to = ()
@@ -34,21 +35,17 @@ def part2():
         if (check_start > 0 and line[check_start] in S):
           is_adj = True
           adj_to = (check_start, index)
-        elif(check_end < len(line) - 1 and line[check_end] in S):
+        elif(check_end < line_end and line[check_end] in S):
           is_adj = True
           adj_to = (check_end, index)
 
         # check above and below
         for i in range(check_start, check_end + 1):
           above = index > 0 and lines[index -1][i] in S
-          below = index < len(line) - 1 and lines[index + 1][i] in S
-          if above:
+          below = index < line_end and lines[index + 1][i] in S
+          if above or below:
             is_adj = True
-            adj_to = (i, index-1)
-            break
-          if below:
-            is_adj = True
-            adj_to = (i, index+1)
+            adj_to = (i, index-1) if above else (i, index+1)
             break
 
         if is_adj:
