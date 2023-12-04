904a7bd296ecc27461ab04c586b33f5b5d902207
ian <ikusner13@gmail.com>
2023-12-04 09:00:36 -0500

    performance metrics

--- day4/p2.py
+++ day4/p2.py
@@ -1,9 +1,12 @@
 from collections import defaultdict
+from memory_profiler import profile
+import time
 
 
 D = open(0).read()
 
 
+@profile
 def part2():
     lines = D.splitlines()
 
@@ -19,13 +22,25 @@ def part2():
         winning_numbers = set(winning_numbers.split())
         my_numbers = my_numbers.split()
 
+        # get the number of winning numbers on the current card
+        # another way to write this would be:
+        # for number in my_numbers:
+        #     if number in winning_numbers:
+        #         winners += 1
         winners = sum(1 for number in my_numbers if number in winning_numbers)
 
+        # loop through the winners and add copies to the next lines
         for i in range(1, winners + 1):
+            # add the number of instances on the current line to the next line
+            # for example, if there are 2 instances of the current card on the current line,
+            # add 2 instances of the next card to the next line
+            # this is because each copy of the current card will add 1 instance of the next card
             instances_per_card[number_on_card + i] += instance_on_current_line
 
     print(sum(instances_per_card.values()))
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     part2()
+    print(f"--- {time.time() - start_time} seconds ---")
