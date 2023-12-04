7c580f608bcf843a8c60f09b2ea7c5dd3527cae9
ian <ikusner13@gmail.com>
2023-12-04 08:48:03 -0500

    done

--- day4/p2.py
+++ day4/p2.py
@@ -4,12 +4,10 @@ from collections import defaultdict
 D = open(0).read()
 
 
-# Part 2
 def part2():
     lines = D.splitlines()
 
     instances_per_card = defaultdict(lambda: 1)
-    winners_per_card = defaultdict(int)
     processed_cards = set()
 
     for current_line, line in enumerate(lines):
@@ -21,19 +19,17 @@ def part2():
 
         number_on_card = int(card_number.split()[1])
         winning_numbers, my_numbers = card_data.split("|")
-        winning_numbers = winning_numbers.split()
+        winning_numbers = set(winning_numbers.split())
         my_numbers = my_numbers.split()
 
         winners = sum(1 for number in my_numbers if number in winning_numbers)
-        winners_per_card[card_number] = winners
 
         for i in range(1, winners + 1):
             instances_per_card[number_on_card + i] += instance_on_current_line
 
         processed_cards.add(card_number)
 
-    sum_instances = sum(instances_per_card.values())
-    print(f"sum_instances: {sum_instances}")
+    print(sum(instances_per_card.values()))
 
 
 if __name__ == "__main__":
