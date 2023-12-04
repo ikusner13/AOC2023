530962e18c970832508e3f0cc34acb8035bee197
ian <ikusner13@gmail.com>
2023-12-04 08:26:43 -0500

    optimize part 2, it was slow as shit

--- day4/p2.py
+++ day4/p2.py
@@ -4,8 +4,8 @@ from collections import defaultdict
 D = open(0).read()
 
 
-# Part 1
-def part1():
+# Part 2
+def part2():
     lines = D.splitlines()
 
     instances_per_card = defaultdict(lambda: 1)
@@ -15,31 +15,26 @@ def part1():
     for current_line, line in enumerate(lines):
         instance_on_current_line = instances_per_card[current_line + 1]
         card_number, card_data = line.split(":")
+
+        if card_number in processed_cards:
+            continue
+
         number_on_card = int(card_number.split()[1])
         winning_numbers, my_numbers = card_data.split("|")
         winning_numbers = winning_numbers.split()
         my_numbers = my_numbers.split()
 
-        while instance_on_current_line > 0:
-            # don't recalculate
-            if winners_per_card.get(current_line, 0) > 0:
-                instance_on_current_line -= 1
-                continue
+        winners = sum(1 for number in my_numbers if number in winning_numbers)
+        winners_per_card[card_number] = winners
 
-            winners = 0
-            for number in my_numbers:
-                if number in winning_numbers:
-                    winners_per_card[card_number] += 1
-                    winners += 1
-                    instances_per_card[number_on_card + winners] += 1
+        for i in range(1, winners + 1):
+            instances_per_card[number_on_card + i] += instance_on_current_line
 
-            instance_on_current_line -= 1
-
-        current_line += 1
+        processed_cards.add(card_number)
 
     sum_instances = sum(instances_per_card.values())
     print(f"sum_instances: {sum_instances}")
 
 
 if __name__ == "__main__":
-    part1()
+    part2()
