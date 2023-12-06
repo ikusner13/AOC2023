165b8a915d95a2e2c778dc4f62e9deb4a01fe72f
Ian Kusner <40447490+ikusner13@users.noreply.github.com>
2023-12-06 09:38:11 -0500

    made quadratic formula work

--- day6/p1.py
+++ day6/p1.py
@@ -13,9 +13,6 @@ def part1():
     times = time_line[1].strip().split()
     distances = distance_lint[1].strip().split()
 
-    print(times)
-    print(distances)
-
     product = 1
     for i in range(len(times)):
         # wait table for 7 ms time:
@@ -34,39 +31,22 @@ def part1():
         # x = (t +- sqrt(t^2 - 4d)) / 2
         # x = (t + sqrt(t^2 - 4d)) / 2
 
-        # x = (7 + sqrt(7^2 - 4*9)) / 2
-        # x = (7 + sqrt(49 - 36)) / 2
-        # x = (7 + sqrt(13)) / 2
-        # x = (7 + 3.60555127546) / 2
-        # x = 5.30277563773
-
-        # x = (7 - sqrt(7^2 - 4*9)) / 2
-        # x = (7 - sqrt(49 - 36)) / 2
-        # x = (7 - sqrt(13)) / 2
-        # x = (7 - 3.60555127546) / 2
-        # x = 1.39722436227
-
         time = int(times[i])
         distance = int(distances[i])
 
-        max_distance = (time / 2) ** 2
+        x1 = (time - math.sqrt(time**2 - 4 * distance)) / 2
+        x2 = (time + math.sqrt(time**2 - 4 * distance)) / 2
 
-        if distance > max_distance:
-            print("Not possible")
-        else:
-            x1 = math.ceil((time - math.sqrt(time**2 - 4 * distance)) / 2)
-            x2 = math.floor((time + math.sqrt(time**2 - 4 * distance)) / 2)
-            ways_to_win = list(range(x1, x2 + 1))
-            print(f"Button hold time range: {x1} to {x2}")
-            print(f"Ways to win: {ways_to_win}")
+        x1 = x1 + 1 if x1 == int(x1) else x1
+        x2 = x2 - 1 if x2 == int(x2) else x2
 
-        w = 0
-        for x in range(time + 1):
-            if x * (time - x) > distance:
-                w += 1
+        x1_rounded = math.ceil(max(0, x1))
+        x2_rounded = math.floor(min(time - 1, x2))
 
-        print(f"Number of ways to win: {w}")
-        product *= w
+        winning_range = x2_rounded - x1_rounded + 1
+
+        print(f"winning_range: {winning_range}")
+        product *= winning_range
 
     print(f"Product: {product}")
 
