4a33cbc5b7c1fc02f83990d21f48c3413e9f4ec4
Ian Kusner <40447490+ikusner13@users.noreply.github.com>
2023-12-07 17:32:02 -0500

    geting closer day 5

--- day5/p2.py
+++ day5/p2.py
@@ -10,8 +10,6 @@ def part2():
 
     maps = list(map(lambda x: x.splitlines(), lines[1::]))
 
-    print(maps)
-
     def create_seed_ranges(seeds):
         seed_ranges = []
         for i in range(0, len(seeds), 2):
@@ -28,55 +26,52 @@ def part2():
         print(f"map: {m[0]}")
         print("#################")
 
-        for rule in mapping_rules:
-            print(f"rule: {rule}")
-            source_range = (int(rule[1]), int(rule[1]) + int(rule[2]) - 1)
-            destination_range = (int(rule[0]), int(rule[0]) + int(rule[2]) - 1)
+        next_ranges = set()
+        for i, input_range in enumerate(input_ranges):
+            in_range = False
+            range_start = input_range[0]
+            range_end = input_range[1]
 
-            next_ranges = []
-            print(f"input_ranges: {input_ranges}")
-            for input_range in input_ranges:
-                # if input range is fully contained in source range
-                if (
-                    input_range[0] >= source_range[0]
-                    and input_range[1] <= source_range[1]
-                ):
-                    range_min = input_range[0] - source_range[0] + destination_range[0]
-                    range_max = input_range[1] - source_range[1] + destination_range[1]
+            for rule in mapping_rules:
+                source_range = (int(rule[1]), int(rule[1]) + int(rule[2]) - 1)
+                destination_range = (int(rule[0]), int(rule[0]) + int(rule[2]) - 1)
 
-                    next_ranges.append((range_min, range_max))
-                    break
-
-                # if input range is partially contained in source range
-                if (
-                    input_range[0] >= source_range[0]
-                    and input_range[0] <= source_range[1]
-                    and input_range[1] >= source_range[1]
-                ):
-                    range_min = input_range[0] - source_range[0] + destination_range[0]
-                    range_max = destination_range[1]
-
-                    next_ranges.append((range_min, range_max))
-
-                if (
-                    input_range[1] >= source_range[0]
-                    and input_range[1] <= source_range[1]
-                    and input_range[0] <= source_range[0]
-                ):
-                    range_min = destination_range[0]
-                    range_max = destination_range[1] - (
-                        source_range[1] - input_range[1]
+                # fully contained
+                if range_start >= source_range[0] and range_end <= source_range[1]:
+                    in_range = True
+                    destination_range_start = (
+                        range_start - source_range[0] + destination_range[0]
+                    )
+                    destination_range_end = (
+                        range_end - source_range[1] + destination_range[1]
                     )
 
-                    next_ranges.append((range_min, range_max))
+                    next_ranges.add((destination_range_start, destination_range_end))
 
-                if len(next_ranges) == 0:
-                    next_ranges.append(input_range)
-            input_ranges = next_ranges
+                # partially contained
+                if (
+                    range_start < source_range[0]
+                    and range_end < source_range[1]
+                    and range_end >= source_range[0]
+                ):
+                    in_range = True
+                    destination_range_start = destination_range[0]
+                    destination_range_end = destination_range[1] - (
+                        source_range[1] - range_end
+                    )
+                    next_ranges.add((destination_range_start, destination_range_end))
 
-    output = input_ranges[0]
+                    new_range_start = range_start
+                    new_range_end = source_range[0] - 1
 
-    print(f"output: {output}")
+                    input_ranges.append((new_range_start, new_range_end))
+
+            if len(next_ranges) == 0 or not in_range:
+                next_ranges.add((range_start, range_end))
+
+        input_ranges = list(next_ranges)
+
+    print(f"min: {min(input_ranges, key=lambda x: x[0])[0]}")
 
 
 if __name__ == "__main__":
