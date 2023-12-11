f0ce450b3e95f9995f0a00888cdeb1481db58632
Ian Kusner <40447490+ikusner13@users.noreply.github.com>
2023-12-11 09:57:55 -0500

    more pythonic

--- day11/p1.py
+++ day11/p1.py
@@ -1,38 +1,31 @@
+from itertools import combinations
+
+
 D = open(0).read().splitlines()
 
 
 def part1():
-    # find empty rows and columns
     empty_rows = {r for r in range(len(D)) if all(cell != "#" for cell in D[r])}
     empty_cols = {
         c for c in range(len(D[0])) if all(D[r][c] != "#" for r in range(len(D)))
     }
 
-    galaxy_pairs = set()
-    for r, row in enumerate(D):
-        for c, col in enumerate(row):
-            if col == "#":
-                row_offset = sum(1 for row in empty_rows if r > row)
-                col_offset = sum(1 for col in empty_cols if c > col)
+    galaxy_pairs = {
+        (
+            r + sum(map(lambda row: r > row, empty_rows)),
+            c + sum(map(lambda col: c > col, empty_cols)),
+        )
+        for r, row in enumerate(D)
+        for c, char in enumerate(row)
+        if char == "#"
+    }
 
-                galaxy_pairs.add((r + row_offset, c + col_offset))
-
-    # find the shortest distance between each galaxy pair
-    distances = {}
-    for galaxy in galaxy_pairs:
-        for galaxy2 in galaxy_pairs:
-            if galaxy == galaxy2:
-                continue
-
-            if (galaxy2, galaxy) in distances or (galaxy, galaxy2) in distances:
-                continue
-
-            x_distance = abs(galaxy[0] - galaxy2[0])
-            y_distance = abs(galaxy[1] - galaxy2[1])
-
-            distances[(galaxy, galaxy2)] = x_distance + y_distance
-
-    print(sum(distances.values()))
+    print(
+        sum(
+            abs(g1[0] - g2[0]) + abs(g1[1] - g2[1])
+            for g1, g2 in combinations(galaxy_pairs, 2)
+        )
+    )
 
 
 if __name__ == "__main__":
