fe23903485c208499093f1a21771ca403ef65daa
Ian Kusner <40447490+ikusner13@users.noreply.github.com>
2023-12-11 09:50:23 -0500

    day11p1

--- day11/p1.py
+++ day11/p1.py
@@ -3,47 +3,19 @@ D = open(0).read().splitlines()
 
 def part1():
     # find empty rows and columns
-    empty_rows = set()
-    empty_cols = set()
-    for c in range(0, len(D)):
-        for r in range(0, len(D[c])):
-            if D[c][r] == "#":
-                break
-        else:
-            empty_rows.add(c)
+    empty_rows = {r for r in range(len(D)) if all(cell != "#" for cell in D[r])}
+    empty_cols = {
+        c for c in range(len(D[0])) if all(D[r][c] != "#" for r in range(len(D)))
+    }
 
-        for r in range(0, len(D[c])):
-            if D[r][c] == "#":
-                break
-        else:
-            empty_cols.add(c)
-
-    """
-    # add an extra row and column to the grid for each empty row and column
-    for ri, row in enumerate(empty_rows):
-        D.insert(ri, "." * len(D[0]))
-    for c in empty_cols:
-        for r in range(0, len(D)):
-            new_row = D[r][:c] + "." + D[r][c:]
-            D[r] = new_row
-
-    print(empty_rows)
-    print(empty_cols)
-    print(D)
-    """
-
-    print(empty_cols)
-    print(empty_rows)
     galaxy_pairs = set()
-    found_galaxies = 0
     for r, row in enumerate(D):
         for c, col in enumerate(row):
-            if col == "#" and (r, c) not in galaxy_pairs:
-                found_galaxies += 1
-                rows_to_add = sum(1 for row in empty_rows if r > row)
-                cols_to_add = sum(1 for col in empty_cols if c > col)
+            if col == "#":
+                row_offset = sum(1 for row in empty_rows if r > row)
+                col_offset = sum(1 for col in empty_cols if c > col)
 
-                galaxy_pairs.add((r + rows_to_add, c + cols_to_add))
+                galaxy_pairs.add((r + row_offset, c + col_offset))
 
     # find the shortest distance between each galaxy pair
     distances = {}
