a24cd21149f7dba8c16586c8a5710ec01f08650b
ian <ikusner13@gmail.com>
2023-12-17 10:33:08 -0500

    d14p2

--- day14/p2.py
+++ day14/p2.py
@@ -1,71 +1,53 @@
-D = open(0).read().splitlines()
-
-
-def part1():
-    total_sum = 0
+def part2():
+    D = open(0).read().splitlines()
 
     def roll(grid):
-        for i in range(len(grid)):
-            if i == 0:
-                continue
-            for ji, j in enumerate(grid[i]):
-                if j == "O":
-                    move_to = i - 1
-                    while grid[move_to][ji] == "." and move_to >= 0:
-                        grid[move_to] = (
-                            grid[move_to][:ji] + "O" + grid[move_to][ji + 1 :]
-                        )
-                        grid[move_to + 1] = (
-                            grid[move_to + 1][:ji] + "." + grid[move_to + 1][ji + 1 :]
-                        )
+        height = len(grid)
+        width = len(grid[0])
 
+        for col in range(width):
+            for row in range(1, height):
+                if grid[row][col] == "O":
+                    move_to = row
+                    while move_to > 0 and grid[move_to - 1][col] == ".":
                         move_to -= 1
+
+                    if move_to != row:
+                        grid[move_to] = (
+                            grid[move_to][:col] + "O" + grid[move_to][col + 1 :]
+                        )
+                        grid[row] = grid[row][:col] + "." + grid[row][col + 1 :]
+
         return grid
 
-    # North
-    n = roll(D)
+    def find_cycle(grid):
+        seen = set()
+        for c in range(1000000000):
+            for _ in range(4):
+                grid = roll(grid)
 
-    print("rolled north")
-    print("\n".join(n))
-    print(" ")
+                grid = list("".join(row) for row in zip(*grid[::-1]))
 
-    print("rotated west")
-    print("\n".join(["".join(row) for row in zip(*n[::-1])]))
-    print(" ")
+                # convert to tuple because is a hashable
+                grid_tuple = tuple(grid)
 
-    # West
-    w = roll(["".join(row) for row in zip(*n[::-1])])
+                if grid_tuple in seen:
+                    return c
+                else:
+                    seen.add(grid_tuple)
+        return 0
 
-    print("rolled west")
-    print("\n".join(w))
-    print(" ")
+    cycle = find_cycle(D)
+    off_by = 1  # had to manually find this, not sure why off by 1
 
-    print("rotated south")
-    print("\n".join([row for row in w[::-1]]))
-    print(" ")
+    for _ in range(1000000000 % (cycle + off_by)):
+        for _ in range(4):
+            D = roll(D)
 
-    # South
-    s = roll([row for row in w[::-1]])
+            D = list("".join(row) for row in zip(*D[::-1]))
 
-    print("rolled south")
-    print("\n".join(s))
-    print(" ")
-
-    print("rotated east")
-    print("\n".join(["".join(row) for row in zip(*s)][::-1]))
-    print(" ")
-    # East
-    e = roll(["".join(row) for row in zip(*s)][::-1])
-
-    print("rolled east")
-    print("\n".join(e))
-    print(" ")
-
-    length = len(e)
-    total_sum += sum(row.count("O") * (length - i) for i, row in enumerate(e))
-
-    print(total_sum)
+    print(sum(row.count("O") * (len(D) - i) for i, row in enumerate(D)))
 
 
 if __name__ == "__main__":
-    part1()
+    part2()
